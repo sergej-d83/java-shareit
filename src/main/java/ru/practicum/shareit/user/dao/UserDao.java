5e6661f68d4069f41423d6d899d2527aee162672
Sergej83 <sergej.d83@yandex.ru>
2022-12-01 16:19:13 +0300

    fix: codestyle issues.

--- src/main/java/ru/practicum/shareit/user/dao/UserDao.java
+++ src/main/java/ru/practicum/shareit/user/dao/UserDao.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface UserDao {
     User addUser(User user);
+
     User getUser(int userId);
+
     Collection<User> getAllUsers();
+
     User updateUser(User user, int userId);
+
     void deleteUser(int userId);
 }
